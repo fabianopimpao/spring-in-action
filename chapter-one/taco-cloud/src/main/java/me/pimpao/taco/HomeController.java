af5420873cb2087f633bf081369b5016945f8e93
fabianopimpao <fabiano.pimpao@projuris.com.br>
2021-01-11 08:00:19 -0300

    Commit of chapter first of the book Spring in action

---
+++ chapter-one/taco-cloud/src/main/java/me/pimpao/taco/HomeController.java
@@ -0,0 +1,14 @@
+package me.pimpao.taco;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(path = "/")
+    public String home() {
+        return "home";
+    }
+
+}
